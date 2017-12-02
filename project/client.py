17dcf3916e4c634834cb7464dc4429e3d62a14ef
Anas Khan <anas.khan96@outlook.com>
2017-12-03 04:19:41 +0530

    updated client.py with cache

--- project/client.py
+++ project/client.py
@@ -32,86 +32,71 @@ class codes(Enum):
 	ERR_KEY_ALREADY_EXISTS = 3
 	ERR_KEY_NOT_RESPONSIBLE = 4
 	ERR_KEY_NOT_FOUND = 5
+    ERR_SERVER_NOT_INIT = 6
 
-def connect_to_server(port_num, isMaster):
+def connect_to_server(request, port_num, isMaster):
     address_append = "/keystore" if not isMaster else "/master"
     address="ws://127.0.0.1:"+port_num+address_append
-    print("address: " + address)
+    print("Address: " + address)
     ws=create_connection(address)
-    request = input().split()
-    #if len(request) > 3:
-        # hack to allow spaces in json value input
-    #	request = [request[0],request[1],''.join(request[2:])]
-    print(request[0])
-    print(request[1])
-    #print(request[2])
-    method, params = request[0].lower(), ""
-    if method == "get":
-        params = {
-            "key": request[1]
-        }
-    elif method == "put":
-        value = ''.join(request[2:])
-        if value[0] == '{':
-            value = literal_eval(value)
+    if request['type'] == "get":
+        if isMaster:
+            listCacheKeys=list(client_cache.keys())
+            for i in listCacheKeys:
+                if i == request['key']:
+                    print("Port associated with key exists in cache")
+                    print("Cache returned port number:", client_cache[i])
+                    connect_to_server(request, str(client_cache[i]), isMaster=False)
+    elif request['type'] == "put":
+        if request['value'][0] == '{':
+            request['value'] = literal_eval(request['value'])
         else:
-            value = value.strip()
-        params = {
-            "key": request[1],
-            "value": value
-        }
-    elif method == "getmultiple":
-        params = {
-            "keys": request[1:]
-        }
+            request['value'] = request['value'].strip()
+    #elif method == "getmultiple":
+    #    params = {
+    #        "keys": request[1:]
+    #    }
     message = {
-        "type": method.lower(),
-        "params": params
+        "type": request['type'],
+        "params": {
+            'key': request['key'],
+            'value': request['value']
+        }
     }
-    #self.sendMessage(json.dumps(message).encode('utf8'))
-    #print("Sending 'Hello, World'...")
     ws.send(json.dumps(message).encode('utf8'))
     print("Sent")
     print("Receiving...")
     result =  ws.recv()
     result = json.loads(result)
-    print(request[1])
-    print("999999")
     print ("Server returned ",result["status"])
-    #print(client_cache[request[1]])
-    if(method=='get'):
-        listCacheKeys=list(client_cache.keys())
-        for i in listCacheKeys:
-            if i==(request[1]):
-                print("recieved locally without master")
-                #value=client_cache[]
-                print("cache returned:",)
-        print ("Server returned data: ",result["data"])
-
-    if(result['status']==codes.SUCCESS.name):
-        client_cache[request[1]]=port_num
+    if(result['status'] == codes.SUCCESS.name):
+        client_cache[request['key']] = port_num
         print(client_cache)  #if the master contains the key. 9001 is the current static master address
 
-    if(result['status'] == codes.ERR_KEY_NOT_RESPONSIBLE.name ):
+    if(result['status'] == codes.ERR_KEY_NOT_RESPONSIBLE.name):
         if "data" in result:
             print("Contact port number "+result["data"])
-            #ws.close()
             new_port_no = result['data']
-            connect_to_server(str(new_port_no), isMaster=False)
+            connect_to_server(request, str(new_port_no), isMaster=False)
             # MASTER SENT NEW PORT NUMBER
         else:
             # CACHED SLAVE NO LONGER EXISTS/HOLDS KEY
             # SO CONTACT MASTER AGAIN
             # PARTHA COME CALL ME WHEN YOU ARE SEEING THIS CODE
-            connect_to_server(str(new_port_no), isMaster=False)
+            connect_to_server(request, MASTER, isMaster=True)
 
     ws.close()
 
-while(True):
-    connect_to_server(MASTER, isMaster=True)
+def send_request(request):
+    request = request.split()
+    request = {
+        'type': request[0].lower(),
+        'key': request[1],
+        'value': ''.join(request[2:]) if len(request) >= 3 else ''
+    }
+    print("Client's request: ",request)
+    while(True):
+        connect_to_server(request, MASTER, isMaster=True)
 
-
-
-
-    #print("Received '%s'" % result)
-    #print("result.status" result.status)
+if __name__ == '__main__':
+    send_request(input())
