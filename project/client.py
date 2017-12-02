1db9551b98f8c8d5f79480e9c98102cfa00a7ffb
rohinrohin <rohinrohin@gmail.com>
2017-12-03 03:16:46 +0530

    milestone #3

--- project/client.py
+++ project/client.py
@@ -1,112 +1,117 @@
+from websocket import create_connection
+import json
 import sys
 import ast
+from enum import Enum
 
-from twisted.internet import reactor
-from twisted.python import log
+MASTER = "8080"
 
-from autobahn.twisted.websocket import WebSocketClientFactory, \
-	WebSocketClientProtocol, \
-	connectWS
-import json
+from collections import OrderedDict
 
+class LimitedSizeDict(OrderedDict):
+  def __init__(self, *args, **kwds):
+    self.size_limit = kwds.pop("size_limit", None)
+    OrderedDict.__init__(self, *args, **kwds)
+    self._check_size_limit()
 
-class ClientProtocol(WebSocketClientProtocol):
+  def __setitem__(self, key, value):
+    OrderedDict.__setitem__(self, key, value)
+    self._check_size_limit()
 
-	def sendHello(self):
-		testput = {
-				"type": "put",
-				"params": {
-						"key": "hello",
-						"value": "world"
-				}
-		}
-		testput2 = {
-				"type": "put",
-				"params": {
-						"key": "hello1",
-						"value": "world1"
-				}
-		}
+  def _check_size_limit(self):
+    if self.size_limit is not None:
+      while len(self) > self.size_limit:
+        self.popitem(last=False)
 
-		testget = {
-				"type": "get",
-				"params": {
-						"key": "hello"
-				}
-		}
-
-		testmultiple = {
-				"type": "getmultiple",
-				"params": {
-						"keys": ["hello", "hello1", "testfail"] 
-				}
-		}
-
-		request = input().split()
-		#if len(request) > 3:
-			# hack to allow spaces in json value input
-		#	request = [request[0],request[1],''.join(request[2:])]
-		method, params = request[0].lower(), ""
-		if method == "get":
-			params = {
-				"key": request[1]
-			}
-		elif method == "put":
-			value = ''.join(request[2:])
-			if value[0] == '{':
-				value = ast.literal_eval(value)
-			else: 
-				value = value.strip()
-			params = {
-				"key": request[1],
-				"value": value
-			}
-		elif method == "getmultiple":
-			params = {
-				"keys": request[1:]
-			}
-
-		message = {
-			"type": method.lower(),
-			"params": params
-		}
-		self.sendMessage(json.dumps(message).encode('utf8'))
-		#self.sendMessage(json.dumps(testput).encode('utf8'))
-		#self.sendMessage(json.dumps(testget).encode('utf8'))
-
-	def onOpen(self):
-		self.sendHello()
-
-	def onClose(self, wasClean, code, reason):
-		print(reason)
-
-	def onMessage(self, payload, isBinary):
-		if not isBinary:
-			print("RESPONSE: {}".format(payload.decode('utf8')))
-		reactor.callLater(1, self.sendHello)
+client_cache= LimitedSizeDict(size_limit=10)
 
 
-class ClientFactory(WebSocketClientFactory):
+class codes(Enum):
+	SUCCESS = 1
+	FAIL = 2
+	ERR_KEY_ALREADY_EXISTS = 3
+	ERR_KEY_NOT_RESPONSIBLE = 4
+	ERR_KEY_NOT_FOUND = 5
 
-	protocol = ClientProtocol
+def connect_to_server(port_num, isMaster):
+    address_append = "/keystore" if not isMaster else "/master"
+    address="ws://127.0.0.1:"+port_num+address_append
+    print("address: " + address)
+    ws=create_connection(address)
+    request = input().split()
+    #if len(request) > 3:
+        # hack to allow spaces in json value input
+    #	request = [request[0],request[1],''.join(request[2:])]
+    print(request[0])
+    print(request[1])
+    #print(request[2])
+    method, params = request[0].lower(), ""
+    if method == "get":
+        params = {
+            "key": request[1]
+        }
+    elif method == "put":
+        value = ''.join(request[2:])
+        if value[0] == '{':
+            value = literal_eval(value)
+        else:
+            value = value.strip()
+        params = {
+            "key": request[1],
+            "value": value
+        }
+    elif method == "getmultiple":
+        params = {
+            "keys": request[1:]
+        }
+    message = {
+        "type": method.lower(),
+        "params": params
+    }
+    #self.sendMessage(json.dumps(message).encode('utf8'))
+    #print("Sending 'Hello, World'...")
+    ws.send(json.dumps(message).encode('utf8'))
+    print("Sent")
+    print("Receiving...")
+    result =  ws.recv()
+    result = json.loads(result)
+    print(request[1])
+    print("999999")
+    print ("Server returned ",result["status"])
+    #print(client_cache[request[1]])
+    if(method=='get'):
+        listCacheKeys=list(client_cache.keys())
+        for i in listCacheKeys:
+            if i==(request[1]):
+                print("recieved locally without master")
+                #value=client_cache[]
+                print("cache returned:",)
+        print ("Server returned data: ",result["data"])
 
-	def clientConnectionLost(self, connector, reason):
-		print(reason)
-		reactor.stop()
+    if(result['status']==codes.SUCCESS.name):
+        client_cache[request[1]]=port_num
+        print(client_cache)  #if the master contains the key. 9001 is the current static master address
 
-	def clientConnectionFailed(self, connector, reason):
-		print(reason)
-		reactor.stop()
+    if(result['status'] == codes.ERR_KEY_NOT_RESPONSIBLE.name ):
+        if "data" in result:
+            print("Contact port number "+result["data"])
+            #ws.close()
+            new_port_no = result['data']
+            connect_to_server(str(new_port_no), isMaster=False)
+            # MASTER SENT NEW PORT NUMBER
+        else:
+            # CACHED SLAVE NO LONGER EXISTS/HOLDS KEY
+            # SO CONTACT MASTER AGAIN
+            # PARTHA COME CALL ME WHEN YOU ARE SEEING THIS CODE
+            connect_to_server(str(new_port_no), isMaster=False)
+
+    ws.close()
+
+while(True):
+    connect_to_server(MASTER, isMaster=True)
 
 
-if __name__ == '__main__':
 
-	if len(sys.argv) < 2:
-		print("Need the WebSocket server address, i.e. ws://127.0.0.1:9000/echo1")
-		sys.exit(1)
 
-	factory = ClientFactory(sys.argv[1])
-	connectWS(factory)
-
-	reactor.run()
-
+    #print("Received '%s'" % result)
+    #print("result.status" result.status)
--- project/server.py
+++ project/server.py
@@ -1,267 +1,356 @@
 import txaio
 from twisted.internet import reactor
 from autobahn.twisted.websocket import WebSocketServerFactory, \
-	WebSocketServerProtocol, \
-	listenWS
+    WebSocketServerProtocol, \
+    listenWS
 from autobahn.websocket.types import ConnectionDeny
 import json
 from enum import Enum
 from kazoo.client import KazooClient
 import time
-from websocket import create_connection
 import math
+from websocket import create_connection
 
 # helpers
 
 class codes(Enum):
-	SUCCESS = 1
-	FAIL = 2
-	ERR_KEY_ALREADY_EXISTS = 3
-	ERR_KEY_NOT_RESPONSIBLE = 4
-	ERR_KEY_NOT_FOUND = 5
+    SUCCESS = 1
+    FAIL = 2
+    ERR_KEY_ALREADY_EXISTS = 3
+    ERR_KEY_NOT_RESPONSIBLE = 4
+    ERR_KEY_NOT_FOUND = 5
+    ERR_SERVER_NOT_INIT = 6
 
 def get_multiple(data, keys):
-	resp = {}
-	for key in keys:
-		if key in data:
-			resp[key] = data[key]
-		else:
-			resp[key] = "ERR_KEY_NOT_FOUND"
-	return resp
+    resp = {}
+    for key in keys:
+        if key in data:
+            resp[key] = data[key]
+        else:
+            resp[key] = "ERR_KEY_NOT_FOUND"
+    return resp
 
 def get(data, key):
-	if key in data:
-		return data[key]
-	else:
-		return codes.ERR_KEY_NOT_FOUND
+    if key in data:
+        return data[key]
+    else:
+        return codes.ERR_KEY_NOT_FOUND
 
 def put(data, key, value):
-	if False:
-		#todo: key-server mapping check
-		return codes.ERR_KEY_NOT_RESPONSIBLE
-	elif key in data:
-		return codes.ERR_KEY_ALREADY_EXISTS
+    if False:
+        #todo: key-server mapping check
+        return codes.ERR_KEY_NOT_RESPONSIBLE
+    elif key in data:
+        return codes.ERR_KEY_ALREADY_EXISTS
 
-	data[key] = value
-	return codes.SUCCESS
+    data[key] = value
+    return codes.SUCCESS
 
 # helpers end
 
 class BaseService:
 
-	def __init__(self, proto):
-		self.proto = proto
-		self.is_closed = False
+    def __init__(self, proto):
+        self.proto = proto
+        self.is_closed = False
 
-	def onOpen(self):
-		pass
+    def onOpen(self):
+        pass
 
-	def onClose(self, wasClean, code, reason):
-		pass
+    def onClose(self, wasClean, code, reason):
+        pass
 
-	def onMessage(self, payload, isBinary):
-		pass
+    def onMessage(self, payload, isBinary):
+        pass
 
 class KeyStoreService(BaseService):
 
-	data = {}
+    data = {}
+    keyRange = {"status": "false", "range": ""}
+    test = 0
 
-	def onMessage(self, payload, isBinary):
-		if not isBinary:
-			payload = json.loads(payload.decode('utf-8'))
-			print("SERVER RECIEVED: " + str(payload))
-			payloadType = payload['type'].upper()
-			payloadParams = payload['params']
-			res = {
-				'status': str(codes.SUCCESS.name).lower()
-			}
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            payload = json.loads(payload.decode('utf-8'))
+            print("SERVER RECIEVED: " + str(payload))
+            payloadType = payload['type'].upper()
+            payloadParams = payload['params']
+            res = {
+                'status': str(codes.SUCCESS.name).lower()
+            }
 
-			if payloadType == 'GET':
-				result = get(self.data, payloadParams['key'])
-				print(result)
-				if result == codes.ERR_KEY_NOT_FOUND:
-					res['status'] = str(result.name)
-				else:
-					res['data'] = result
+            if payloadType == 'SETKEY':
+                print("MASTER KEYSET RECIEVED")
+                self.keyRange["range"] = payloadParams['data']
+                self.keyRange["status"] = "true"
+                print("KEYRANGE: ", self.keyRange)
+                msg = json.dumps(res)
+                print("SERVER SENT: " + msg)
+                self.proto.sendMessage(msg.encode('utf8'))
+                return
 
-			elif payloadType == 'GETMULTIPLE':
-				result = {}
-				for key in payloadParams['keys']:
-					temp = get(self.data, key)
-					if temp == codes.ERR_KEY_NOT_FOUND:
-						result[key] = str(temp.name)
-					else:
-						result[key] = temp
-					res['data'] = result
+            if self.keyRange["status"] == "false":
+                res['status'] = str(codes.ERR_SERVER_NOT_INIT.name).lower()
 
-			elif payloadType == 'PUT':
-				result = put(self.data, payloadParams['key'], payloadParams['value'])
-				if result == codes.ERR_KEY_NOT_RESPONSIBLE or result == codes.ERR_KEY_ALREADY_EXISTS:
-					res['status'] = str(result.name)
+            if payloadType == 'GET':
+                result = get(self.data, payloadParams['key'])
+                print(result)
+                if result == codes.ERR_KEY_NOT_FOUND:
+                    res['status'] = str(result.name)
+                else:
+                    res['data'] = result
 
-			elif payloadType == 'REPLICA':
-				res['data'] = self.data
+            elif payloadType == 'GETMULTIPLE':
+                result = {}
+                for key in payloadParams['keys']:
+                    temp = get(self.data, key)
+                    if temp == codes.ERR_KEY_NOT_FOUND:
+                        result[key] = str(temp.name)
+                    else:
+                        result[key] = temp
+                    res['data'] = result
 
-			else:
-				res['status'] = str(codes.FAIL.name)
-				res['data'] = "Operation not permitted"
+            elif payloadType == 'PUT':
+                firstChar = ord(payloadParams['key'][0])
+                start, end = self.keyRange['range'].split('-')
+                start, end = int(start), int(end)
+                if firstChar > start and firstChar <= end:
+                    result = put(self.data, payloadParams['key'], payloadParams['value'])
+                else:
+                    res['status'] = codes.ERR_KEY_NOT_RESPONSIBLE
 
-			msg = json.dumps(res)
-			print("SERVER SENT: " + msg)
-			self.proto.sendMessage(msg.encode('utf8'))
+            elif payloadType == 'REPLICA':
+                res['data'] = self.data
+
+
+            else:
+                res['status'] = str(codes.FAIL.name)
+                res['data'] = "Operation not permitted"
+
+            msg = json.dumps(res)
+            print("SERVER SENT: " + msg)
+            self.proto.sendMessage(msg.encode('utf8'))
 
 class MasterService(BaseService):
 
-	metadata = {}
-	data = {}
-	keyRange = ''
-	keyRanges = {}
+    metadata = {}
+    data = {}
+    keyRange = {"status": "false", "range": ""}
+    keyRanges = {}
 
-	def onMessage(self, payload, isBinary):
-		if not isBinary:
-			msg = str(self.data) + "Echo master handler - {}".format(payload.decode('utf8'))
-			print(msg)
-			self.proto.sendMessage(msg.encode('utf8'))
+    def checkKeyRange(self, key):
+        start, end = self.keyRange['range'].split('-')
+        start, end = int(start), int(end)
+        firstChar = ord(key[0])
+        if not (firstChar > start and firstChar <= end):
+            for key in self.keyRanges:
+                start, end = key.split('-')
+                start, end = int(start), int(end)
+                if firstChar > start and firstChar <= end:
+                    return self.keyRanges[key]
+
+        return (codes.SUCCESS)
+
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            payload = json.loads(payload.decode('utf-8'))
+            print("MASTER SERVER RECEIVED: " + str(payload))
+            payloadType = payload['type'].upper()
+            payloadParams = payload['params']
+            res = {
+                'status': str(codes.SUCCESS.name).lower()
+            }
+
+            if self.keyRange["status"] == "false":
+                res['status'] = str(codes.ERR_SERVER_NOT_INIT.name).lower()
+
+            if payloadType == 'GET':
+                keyRangeCheck = self.checkKeyRange(payloadParams['key'])
+                if keyRangeCheck == codes.SUCCESS:
+                    # key in master
+                    result = get(self.data, payloadParams['key'])
+                    if result == codes.ERR_KEY_NOT_FOUND:
+                        # key supposed to be in master but not found
+                        res['status'] = str(result.name)
+                    else:
+                        res['data'] = result
+                else:
+                    # key not responsible
+                    res["status"] = str(codes.ERR_KEY_NOT_RESPONSIBLE.name)
+                    res["data"] = str(keyRangeCheck)
+                print(res)
+
+            #elif payloadType == 'GETMULTIPLE':
+            #    result = {}
+            #    for key in payloadParams['keys']:
+            #        temp = get(self.data, key)
+            #        if temp == codes.ERR_KEY_NOT_FOUND:
+            #            result[key] = str(temp.name)
+            #        else:
+            #            result[key] = temp
+            #        res['data'] = result
+
+            elif payloadType == 'PUT':
+                keyRangeCheck = self.checkKeyRange(payloadParams['key'])
+                if keyRangeCheck == codes.SUCCESS:
+                    # key in master
+                    result = put(self.data, payloadParams['key'], payloadParams['value'])
+                    if result == codes.ERR_KEY_ALREADY_EXISTS:
+                        res['status'] = str(result.name)
+                else:
+                    res["status"] = str(codes.ERR_KEY_NOT_RESPONSIBLE.name)
+                    res["data"] = str(keyRangeCheck)
+
+            elif payloadType == 'REPLICA':
+                res['data'] = self.data
+
+
+            else:
+                res['status'] = str(codes.FAIL.name)
+                res['data'] = "Operation not permitted"
+
+            msg = json.dumps(res)
+            print("SERVER SENT: " + msg)
+            self.proto.sendMessage(msg.encode('utf8'))
 
 class BackupKeyStoreService(BaseService):
 
-	data = {}
+    data = {}
 
-	def onMessage(self, payload, isBinary):
-		if not isBinary:
-			msg = "Echo 2 - {}".format(payload.decode('utf8'))
-			print(msg)
-			self.proto.sendMessage(msg.encode('utf8'))
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            msg = "Echo 2 - {}".format(payload.decode('utf8'))
+            print(msg)
+            self.proto.sendMessage(msg.encode('utf8'))
 
 
 class ServiceServerProtocol(WebSocketServerProtocol):
 
-	SERVICEMAP = {
-		'/master': MasterService,
-		'/keystore': KeyStoreService,
-		'/backup': BackupKeyStoreService
-	}
+    SERVICEMAP = {
+        '/master': MasterService,
+        '/keystore': KeyStoreService,
+        '/backup': BackupKeyStoreService
+    }
 
-	def __init__(self):
-		self.service = None
-		self.is_closed = txaio.create_future()
+    def __init__(self):
+        self.service = None
+        self.is_closed = txaio.create_future()
 
-	def onConnect(self, request):
-		# request has all the information from the initial
-		# WebSocket opening handshake ..
-		#print(request.peer)
-		#print(request.headers)
-		#print(request.host)
-		#print(request.path)
-		#print(request.params)
-		#print(request.version)
-		#print(request.origin)
-		#print(request.protocols)
-		#print(request.extensions)
+    def onConnect(self, request):
+        # request has all the information from the initial
+        # WebSocket opening handshake ..
+        #print(request.peer)
+        #print(request.headers)
+        #print(request.host)
+        #print(request.path)
+        #print(request.params)
+        #print(request.version)
+        #print(request.origin)
+        #print(request.protocols)
+        #print(request.extensions)
 
-		if request.path in self.SERVICEMAP:
-			cls = self.SERVICEMAP[request.path]
-			self.service = cls(self)
-		else:
-			err = "No service under %s" % request.path
-			print(err)
-			raise ConnectionDeny(404, unicode(err))
+        if request.path in self.SERVICEMAP:
+            cls = self.SERVICEMAP[request.path]
+            self.service = cls(self)
+        else:
+            err = "No service under %s" % request.path
+            print(err)
+            raise ConnectionDeny(404, unicode(err))
 
-	def onOpen(self):
-		if self.service:
-			self.service.onOpen()
+    def onOpen(self):
+        if self.service:
+            self.service.onOpen()
 
-	def onMessage(self, payload, isBinary):
-		if type(self.service).__name__ == 'MasterService':
-			self.service.onMessage(payload, isBinary)
-		elif self.service:
-			self.service.onMessage(payload, isBinary)
+    def onMessage(self, payload, isBinary):
+        if type(self.service).__name__ == 'MasterService':
+            self.service.onMessage(payload, isBinary)
+        elif self.service:
+            self.service.onMessage(payload, isBinary)
 
-	def onClose(self, wasClean, code, reason):
-		if self.service:
-			self.service.onClose(wasClean, code, reason)
+    def onClose(self, wasClean, code, reason):
+        if self.service:
+            self.service.onClose(wasClean, code, reason)
 
 
 if __name__ == '__main__':
 
-	zk = KazooClient(hosts='127.0.0.1:2181')
-	zk.start()
+    zk = KazooClient(hosts='192.168.31.233:2181')
+    zk.start()
 
-	import logging
-	logging.basicConfig()
+    import logging
+    logging.basicConfig()
 
-	currInstance = zk.create('/app/instance', ephemeral=True, sequence=True, makepath=True)
-	print(currInstance, " started. ")
+    currInstance = zk.create('/app/instance', ephemeral=True, sequence=True, makepath=True)
+    print(currInstance, " started. ")
 
-	children = zk.get_children('/app')
-	print(children)
+    children = zk.get_children('/app')
+    print(children)
 
-	portno = 0
+    portno = 0
 
-	if len(children) == 1:
-			portno = 8080
-			print("MASTER")
-			if zk.exists('/meta'):
-				zk.delete('/meta', recursive=True)
+    if len(children) == 1:
+            portno = 8080
+            print("MASTER")
+            if zk.exists('/meta'):
+                zk.delete('/meta', recursive=True)
 
-			zk.create('/meta/master',b'8080', makepath=True)
-			zk.create('/meta/status',b'initializing', makepath=True)
-			zk.create('/meta/lastport','8080'.encode('utf-8'), makepath=True)
-			time.sleep(20)
-			children = zk.get_children('/app')
-			config = {
-				"lastDead": {
-					"backup": -1,
-					# backup is the server port whose data the dead server needs to backuo
-					"keystore": -1
-					# keystore is the server whose /backup it has to read from
-				},
-				"numOfServers": len(children)
-			}
-			zk.create('/meta/config', json.dumps(config).encode('utf-8'))
-			print('Server init')
-			print(children)
-			totalKeyRange = 122 - 48 + 1
-			individualRange = int(math.floor(totalKeyRange / len(children)))
-			ranges = []
-			start = 48
-			for i in range(len(children)):
-				ranges.append(str(start) + "-" + str(start+individualRange))
-				start = start + individualRange + 1
-			ranges[-1] = ranges[-1].split("-")[0] + "-122" # change this later please
-			print(ranges)
-			MasterService.keyRange = ranges[0]
+            zk.create('/meta/master',b'8080', makepath=True)
+            zk.create('/meta/status',b'initializing', makepath=True)
+            zk.create('/meta/lastport','8080'.encode('utf-8'), makepath=True)
+            time.sleep(5)
+            children = zk.get_children('/app')
+            config = {
+                "lastDead": {
+                    "backup": -1,
+                    # backup is the server port whose data the dead server needs to backuo
+                    "keystore": -1
+                    # keystore is the server whose /backup it has to read from
+                },
+                "numOfServers": len(children)
+            }
+            zk.create('/meta/config', json.dumps(config).encode('utf-8'))
+            print('Server init')
+            print(children)
+            totalKeyRange = 122 - 48 + 1
+            individualRange = int(math.floor(totalKeyRange / len(children)))
+            ranges = []
+            start = 48
+            for i in range(len(children)):
+                ranges.append(str(start) + "-" + str(start+individualRange))
+                start = start + individualRange + 1
+            ranges[-1] = ranges[-1].split("-")[0] + "-122" # change this later please
+            print(ranges)
+            MasterService.keyRange["range"] = ranges[0]
+            MasterService.keyRange["status"] == "True"
 
-			portnum, _ = zk.get('/meta/lastport')
-			portnum = int(portnum.decode('utf-8'))
-			for port in range(8081, portnum + 1):
-				MasterService.keyRanges[ranges[port - 8080]] = port
-				signal = {
-					"status": "ready",
-					"data": ranges[port - 8080]
-				}
-				#ws = create_connection("ws://127.0.0.1:%s/config" % str(port))
-				#print('Sending to port %s' % str(port))
-				#ws.send(json.dumps(signal))
+            portnum, _ = zk.get('/meta/lastport')
+            portnum = int(portnum.decode('utf-8'))
+            for port in range(8081, portnum + 1):
+               MasterService.keyRanges[ranges[port - 8080]] = port
 
+               signal = {
+                   "type": "setkey",
+                   "params": {"data": ranges[port - 8080]}
+               }
 
-	else:
-		print("SLAVE")
-		portno, _ = zk.get('/meta/lastport')
-		portno = int(portno.decode('utf-8')) + 1
-		zk.set('/meta/lastport', str(portno).encode())
-		#ws = create_connection("ws://127.0.0.1:%s" % str(portno))
-		#print("SLAVE: WAITING FOR SIGNAL ON %s" % str(portno))
-		#result =  ws.recv()
-		#result = json.loads(result)
-		#if result["status"] == codes.READY.name:
-		#	KeyStoreService.keyRange = result["data"]
-		#print("SLAVE: READY FOR KEYRANGE " + result["data"])
-		#ws.close()
+               ws = create_connection("ws://127.0.0.1:" + str(port) + "/keystore")
+               print(json.dumps(signal))
+               ws.send(json.dumps(signal))
+               print ("Sent")
+               print ("Receiving...")
+               result =  ws.recv()
+               print ("Received '%s'" % result)
+               ws.close()
 
+    else:
+        print("SLAVE")
+        portno, _ = zk.get('/meta/lastport')
+        portno = int(portno.decode('utf-8')) + 1
+        zk.set('/meta/lastport', str(portno).encode())
+        print("SLAVE LISTENING FOR MASTER KEYSET")
 
-	factory = WebSocketServerFactory(u"ws://127.0.0.1:%s" % str(portno))
-	factory.protocol = ServiceServerProtocol
-	listenWS(factory)
+    factory = WebSocketServerFactory(u"ws://127.0.0.1:%s" % str(portno))
+    factory.protocol = ServiceServerProtocol
+    listenWS(factory)
 
-	reactor.run()
+    reactor.run()
